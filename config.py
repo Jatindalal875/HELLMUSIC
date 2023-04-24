6fc7be2ed20b818e88e460e927813f49111b435d
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2023-04-24 23:36:30 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -9,19 +9,19 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "900000000000000"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/bd72db7afa8a08a916d49.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/9a246bd831590d22d2582.jpg")
 
 SESSION = getenv("SESSION", None)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/EAGLE_MAFIA_CLUB")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/THE_BROTHERHOOD_COUNCIL")
+CHAT_GROUP = getenv("CHAT_GRP", "https://t.me/EAGLE_MAFIA_CLUB")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1864894033").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5388906652").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
